371fc9516b9ad345cf6754670cfed91d697d925f
agoodson582 <agoodson582@gmail.com>
2019-12-24 22:16:41 -0500

    Added form functionality to cart page

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+
 import {CartService} from '../cart.service';
 
 @Component({
@@ -9,10 +11,25 @@ import {CartService} from '../cart.service';
 
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    });
+  }
+
+  onSubmit(customerData) {
+    // Checkout data would be processed here
+    console.warn('Your order has been submitted.', customerData);
+    alert('Your order has been placed! Thank you very much!');
+
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
   }
 }
